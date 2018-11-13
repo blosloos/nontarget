12acc2e96d7d7e30c489a64f1029452a514b0cfa
blosloos <blosloos@users.noreply.github.com>
2018-11-13 13:40:06 +0100

    version number

--- R/nontarget.R
+++ R/nontarget.R
@@ -1,7 +1,7 @@
 .onAttach <- function(lib, pkg)
 {
 
-	packageStartupMessage("\n \n Welcome to nontarget version 1.9. Please cite this package as:\n
+	packageStartupMessage("\n \n Welcome to nontarget version 2.0. Please cite this package as:\n
 	Loos, M., Singer, H. (2017). Nontargeted homologue series extraction from hyphenated high resolution mass spectrometry, Journal of Cheminformatics 9(12).\n\n");
 
 }
